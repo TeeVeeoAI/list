aa165c239f009a1091627eca18495db2309d84c1
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-12-06 13:55:06 +0100

    +

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+List<List<char>> l = new List<List<char>>();
+
+List<char> c1 = new List<char>();
+List<char> c2 = new List<char>();
+List<char> c3 = new List<char>();
+List<char> c4 = new List<char>();
+
+c1.Add('t');
+c1.Add('h');
+c2.Add('5');
+c3.Add('r');
+c4.Add('g');
+
+l.Add(c1);
+l.Add(c2);
+l.Add(c3);
+l.Add(c4);
+
+int i = 1;
+
+foreach (List<char> cl in l) {
+    Console.WriteLine("Char list " + i);
+    foreach (char C in cl) {
+        if (cl.IndexOf(C) == cl.Count - 1)
+            Console.WriteLine(C);
+        else
+            Console.Write(C + ", ");
+    }
+    i++;
+}
